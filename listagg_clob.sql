b7dd64808fefb17cade6e3ed3d63852dfe7dd457
juliusbsimon <50272257+juliusbsimon@users.noreply.github.com>
2025-08-07 10:28:00 -0400

    Update listagg_clob.sql

--- listagg_clob.sql
+++ listagg_clob.sql
@@ -3,3 +3,16 @@ return clob
 parallel_enable aggregate using listagg_clob_t;
 /
 
+CREATE OR REPLACE FUNCTION listagg_clob_with_sep(
+  p_val      VARCHAR2,
+  p_separator VARCHAR2
+) RETURN CLOB
+IS
+  l_raw_clob CLOB;
+BEGIN
+  -- call the aggregate with its normal single argument
+  l_raw_clob := listagg_clob(p_val);
+  -- replace the unique token with the requested separator
+  RETURN REPLACE(l_raw_clob, '#####', p_separator);
+END;
+/
